4c8f2bde16d446c6b3b97c9b444c502c8e46f893
YunaLin <linzy28@mail2.sysu.edu.cn>
2018-03-29 21:27:08 +0800

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <string>
-#include <fstream>
 #include <vector>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <cmath>
 #include "errno.h"
 #include <time.h>
-#include "../include/ProcessPool.h"
+#include "ProcessPool.h"
 
 using namespace std;
+
 void primeSieve(int number);
-clock_t start;
+void printSubResult(vector<bool> nums, int index, int low_bound, int start);
 
 int main() {
     int num;
@@ -31,77 +31,79 @@ void primeSieve(int number) {
     static ProcessPool* mypool = ProcessPool::createPool(process_number);
     int loc = 0;
 
-    if (mypool -> getIndex() < 0) {
-    //这个进程代表的是父进程
-    int father_range = total / (process_number + 1) + total % (process_number + 1) + 1;
-    vector<bool> num(father_range, true);
-    num[0] = num[1] = false;
-    for (int i = 2; i < father_range; i++) {
-        loc = i;
-        if (num[i]) {
-            for (int j = 0; j < process_number; j++) {
-              write(mypool->getSubProcess()[j].getfds()[1], &loc, 4);
-            }
-            loc = i + i;
-            while (loc < father_range) {
-              num[loc] = false;
-              loc += i;
+    if (mypool->getIndex() < 0) {
+        //这个进程代表的是父进程
+        int father_range = total / (process_number + 1) + total % (process_number + 1) + 1;
+        vector<bool> prime(father_range, true);
+        prime[0] = prime[1] = false;
+
+        for (int i = 2; i < father_range; i++) {
+            loc = i;
+            if (prime[i]) {
+                for (int j = 0; j < process_number; j++) {
+                  write(mypool->getSubProcess()[j].getfiledes()[1], &loc, 4);
+                }
+                loc = i + i;
+                while (loc < father_range) {
+                  prime[loc] = false;
+                  loc += i;
+                }
             }
         }
-    }
-    for (int i = 0; i < process_number; i++) {
-      close(mypool->getSubProcess()[i].getfds()[1]);
-    }
+        for (int i = 0; i < process_number; i++) {
+          close(mypool->getSubProcess()[i].getfiledes()[1]);
+        }
 
-    //等待所有的子进程全部结束
-    int size = num.size();
-    cout << "process 0: ";
-    for (int i = 0; i < size; i++) {
-      if (num[i]) {
-        cout << i << " ";
-      }
-    }
-    cout << endl;
-    int status;
-    pid_t wpid;
-    while ((wpid = wait(&status)) > 0);
+        //等待所有的子进程全部结束
+        int size = prime.size();
+        cout << "process 0: ";
+        for (int i = 0; i < size; i++) {
+          if (prime[i]) {
+            cout << i << " ";
+          }
+        }
+        cout << endl << endl;
+        int status;
+        pid_t wpid;
+        while ((wpid = wait(&status)) > 0);
         clock_t now = clock();
-        int processId = mypool -> getIndex() + process_number + 1;
-        clock_t start = mypool -> getSubProcess()[processId].getBeginTime();
-        double time = (double)(now - start) / CLOCKS_PER_SEC;
+        int processId = mypool->getIndex() + process_number + 1;
+        clock_t begin = mypool->getSubProcess()[processId].getBeginTime();
+        double time = (double)(now - begin) / CLOCKS_PER_SEC;
         cout << "Totaltime: " << time << endl << endl;
     } else {
-        int index = mypool -> getIndex();
+        int index = mypool->getIndex();
         int low_bound = total / (process_number + 1) * (index + 1) + total % (process_number + 1) + 1;
         int sub_range = total / (process_number + 1);
-        int* step = new int();
         vector<bool> nums(sub_range, true);
         while (1) {
-          //这里是如何实现选择到不同的值的
-          int size = read(mypool->getSubProcess()[index].getfds()[0], step, 4);
-          if (size > 0) {
-            //需要找出的是第一个能被*step整除的数
-            int start = 0;
-            start = (low_bound / (*step)) * (*step) == low_bound ? low_bound : (low_bound / (*step)) * (*step) + (*step);
-            for (int i = start; i < low_bound + sub_range;) {
-              nums[i - low_bound] = false;
-              i += *step;
+            //这里是如何实现选择到不同的值的
+            int size = read(mypool->getSubProcess()[index].getfiledes()[0], step, 4);
+            if (size > 0) {
+                //需要找出的是第一个能被*step整除的数
+                int start = 0;
+                start = (low_bound / (*step)) * (*step) == low_bound ? low_bound : (low_bound / (*step)) * (*step) + (*step);
+                for (int i = start; i < low_bound + sub_range;) {
+                    nums[i - low_bound] = false;
+                    i += *step;
+                }
+            } else {
+                break;
             }
-          } else {
-            break;
-          }
+        }
+        clock_t start = mypool -> getSubProcess()[index].getBeginTime();
+        printSubResult(nums, index, low_bound, start);
     }
-    int size = nums.size();
+}
+void printSubResult(vector<bool> nums, int index, int low_bound, int start) {
+    int n = nums.size();
     cout << "process " << index << ": ";
-    for (int i = 0; i < size; i++) {
-      if (nums[i]) {
-        cout << i + low_bound << " ";
-      }
+    for (int i = 0; i < n; i++) {
+        if (nums[i])
+            cout << i + low_bound << " ";
     }
     cout << endl;
     clock_t now = clock();
-    clock_t start = mypool -> getSubProcess()[index].getBeginTime();
     double time = (double)(now - start) / CLOCKS_PER_SEC;
     cout << "time: " << time << endl << endl;
-    }
 }
